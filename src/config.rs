8dec0f3187283077f01eac55b64ea1a3adcc71d4
Yuki Kishimoto <yukikishimoto@protonmail.com>
2025-09-22 10:45:31 -0300

    Simplify `Config` struct by removing unused fields and refactoring initialization methods

--- src/config.rs
+++ src/config.rs
@@ -1,71 +1,38 @@
-#[derive(Clone, Debug)]
-pub struct Config {
-    pub rest_api_endpoint: String,
-    pub ws_endpoint: String,
-
-    pub futures_rest_api_endpoint: String,
-    pub futures_ws_endpoint: String,
-
-    pub recv_window: u64,
-}
-
 pub const SPOT_MAINNET: &str = "https://api.binance.com";
 pub const SPOT_TESTNET: &str = "https://testnet.binance.vision";
+const DEFAULT_RECV_WINDOW: u64 = 5000;
 
-pub const SPOT_WS_MAINNET: &str = "wss://stream.binance.com/ws";
-pub const SPOT_WS_TESTNET: &str = "wss://testnet.binance.vision/ws";
-
-pub const FUTURES_MAINNET: &str = "https://fapi.binance.com";
-pub const FUTURES_TESTNET: &str = "https://testnet.binancefuture.com";
-
-pub const FUTURES_WS_MAINNET: &str = "wss://fstream.binance.com/ws";
-pub const FUTURES_WS_TESTNET: &str = "wss://fstream.binancefuture.com/ws";
+#[derive(Debug, Clone)]
+pub struct Config {
+    pub rest_api_endpoint: String,
+    pub recv_window: u64,
+}
 
 impl Default for Config {
     fn default() -> Self {
         Self {
-            rest_api_endpoint: SPOT_MAINNET.into(),
-            ws_endpoint: SPOT_WS_MAINNET.into(),
-
-            futures_rest_api_endpoint: FUTURES_MAINNET.into(),
-            futures_ws_endpoint: FUTURES_WS_MAINNET.into(),
-
-            recv_window: 5000,
+            rest_api_endpoint: SPOT_MAINNET.to_string(),
+            recv_window: DEFAULT_RECV_WINDOW,
         }
     }
 }
 
 impl Config {
+    #[inline]
     pub fn testnet() -> Self {
-        Self::default()
-            .set_rest_api_endpoint(SPOT_TESTNET)
-            .set_ws_endpoint(SPOT_WS_TESTNET)
-            .set_futures_rest_api_endpoint(FUTURES_TESTNET)
-            .set_futures_ws_endpoint(FUTURES_WS_TESTNET)
+        Self::default().set_rest_api_endpoint(SPOT_TESTNET)
     }
 
-    pub fn set_rest_api_endpoint<T: Into<String>>(mut self, rest_api_endpoint: T) -> Self {
+    #[inline]
+    pub fn set_rest_api_endpoint<T>(mut self, rest_api_endpoint: T) -> Self
+    where
+        T: Into<String>,
+    {
         self.rest_api_endpoint = rest_api_endpoint.into();
         self
     }
 
-    pub fn set_ws_endpoint<T: Into<String>>(mut self, ws_endpoint: T) -> Self {
-        self.ws_endpoint = ws_endpoint.into();
-        self
-    }
-    pub fn set_futures_rest_api_endpoint<T: Into<String>>(
-        mut self,
-        futures_rest_api_endpoint: T,
-    ) -> Self {
-        self.futures_rest_api_endpoint = futures_rest_api_endpoint.into();
-        self
-    }
-
-    pub fn set_futures_ws_endpoint<T: Into<String>>(mut self, futures_ws_endpoint: T) -> Self {
-        self.futures_ws_endpoint = futures_ws_endpoint.into();
-        self
-    }
-
+    #[inline]
     pub fn set_recv_window(mut self, recv_window: u64) -> Self {
         self.recv_window = recv_window;
         self
