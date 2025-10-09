4baa3e0009fc399bb0d22933ff893da874e0ac52
Yuki Kishimoto <yukikishimoto@protonmail.com>
2025-10-09 11:28:45 -0300

    Reduce default timeout to 25 secs

--- src/constant.rs
+++ src/constant.rs
@@ -5,4 +5,4 @@ pub(crate) const SPOT_MAINNET_US: &str = "https://api.binance.us";
 pub(crate) const SPOT_TESTNET: &str = "https://testnet.binance.vision";
 
 pub(crate) const DEFAULT_RECV_WINDOW: u64 = 5000;
-pub(crate) const DEFAULT_TIMEOUT: Duration = Duration::from_secs(60);
+pub(crate) const DEFAULT_TIMEOUT: Duration = Duration::from_secs(25);
